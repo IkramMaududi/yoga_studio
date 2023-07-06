8b0b858c4393c8efb987b01e01053958b081e012
sonya <nethania.s.sihombing@accenture.com>
2023-07-06 15:33:57 +0700

    add about page

--- src/pages/Home-About/About.js
+++ src/pages/Home-About/About.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import './About.css';
+import { FaCheckCircle } from 'react-icons/fa';
 
-function About() {
+import './About.css';
+import yogaImage from '../../assets/yoga-image.jpg';
+import member1Image from '../../assets/member1.jpg';
+import member2Image from '../../assets/member2.jpg';
+import member3Image from '../../assets/member3.jpg';
+import member4Image from '../../assets/member4.jpg';
+import member5Image from '../../assets/member5.jpg';
+import member6Image from '../../assets/member6.jpg';
+
+class About extends React.Component {
+  render() {
+    const reasons = [
+        { icon: <FaCheckCircle />, title: 'Experienced and Knowledgeable Instructors', description: 'Personalized guidance and expert instruction for your practice.' },
+        { icon: <FaCheckCircle />, title: 'Welcoming and Inclusive Community', description: 'Belonging and acceptance in a diverse and supportive environment.' },
+        { icon: <FaCheckCircle />, title: 'Variety of Classes and Workshops', description: 'Diverse options for all levels and interests.' }
+    ];
+
+    const teamMembers = [
+        { name: 'Budi Santoso', role: 'Yoga Instructor', email: 'budi.santoso@yogastudio.com', image: member1Image },
+        { name: 'Siti Rahayu', role: 'Yoga Teacher', email: 'siti.rahayu@yogastudio.com', image: member2Image },
+        { name: 'Joko Susanto', role: 'Yoga Trainer', email: 'joko.susanto@yogastudio.com', image: member3Image },
+        { name: 'Rini Wahyuni', role: 'Yoga Specialist', email: 'rini.wahyuni@yogastudio.com', image: member4Image },
+        { name: 'Ahmad Pratama', role: 'Yoga Consultant', email: 'ahmad.pratama@yogastudio.com', image: member5Image },
+        { name: 'Dewi Sari', role: 'Yoga Advisor', email: 'dewi.sari@yogastudio.com', image: member6Image },
+      ];
+
     return (
-        <div id="top" className="fullSize">
-            <div className="all"> 
-                <div className="about-section-1">
-                    <div className="inner-container">
-                        <h1>About Us</h1>
-                        <p className="text">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum, eligendi nostrum pariatur necessitatibus eius dicta a voluptates. 
-                        </p>
-                        <div className="skills">
-                            <span>Web Design</span>
-                            <span>Photoshop & Illustrator</span>
-                            <span>Coding</span>
-                       </div>
+      <div className="About">
+        <section className="why-choose-us">
+            <div className="reasons">
+                <h2>Why Choose Us</h2>
+                <div className="reason-list">
+                    {reasons.map((reason, index) => (
+                        <div className="reason" key={index}>
+                        <div className="icon">{reason.icon}</div>
+                        <div className="content">
+                            <h3>{reason.title}</h3>
+                            <p>{reason.description}</p>
+                        </div>
+                        </div>
+                    ))}
+                </div>
+          </div>
+          <div className="yoga-image">
+            <img src={yogaImage} alt="Yoga" />
+          </div>
+        </section>
+        <section className="our-team">
+          <h2>Our Team</h2>
+          <div className="team-members">
+            {teamMembers.map((member, index) => (
+                <div className="member" key={index}>
+                    <img src={member.image} alt={member.name} />
+                    <div className="member-info">
+                        <h3>{member.name}</h3>
+                        <p>{member.role}</p>
+                        <p>{member.email}</p>
                     </div>
                 </div>
-            </div>
-        </div>
+            ))}
+          </div>
+        </section>
+      </div>
     );
-};
+  }
+}
 
-export default About; 
+export default About;
